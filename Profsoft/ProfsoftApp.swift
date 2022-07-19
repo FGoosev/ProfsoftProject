75f170b56db4b0041452b840ba3e63b174768c23
FGoosev <FGoosev@gmail.com>
2022-07-19 18:22:58 +0400

    Initial Commit

---
+++ Profsoft/ProfsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfsoftApp.swift
+//  Profsoft
+//
+//  Created by Alexandr Gusev on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProfsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
